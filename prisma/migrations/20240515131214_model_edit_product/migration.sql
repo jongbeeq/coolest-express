9dfa0b92825d42372ac896def0e81b616fb81c1e
jongbeeq <titivutewhangsukij@gmail.com>
2024-05-15 20:12:35 +0700

    Model:editProduct

---
+++ prisma/migrations/20240515131214_model_edit_product/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `product` MODIFY `description` VARCHAR(600) NULL,
+    MODIFY `categoryId` INTEGER NULL,
+    MODIFY `maxPrice` INTEGER NULL;
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,9 +1,12 @@
 const { uploadToCloud } = require('../utils/cloudinary-service')
 const fs = require('fs/promises')
 const createError = require('../utils/create-error')
+const prisma = require('../models/prisma')
 
 exports.createProduct = async (req, res, next) => {
     try {
+        console.log(req.body)
+        console.log(req.files)
         const uploadingFiles = req.files.map(file => {
             const type = file.mimetype.split('/')[0]
             return uploadToCloud(file.path, type)
@@ -11,11 +14,17 @@ exports.createProduct = async (req, res, next) => {
 
         const allFiles = await Promise.all(uploadingFiles)
 
-
-        // console.log(allFiles)
-
-        // res.status(200).json(allFiles)
-        res.status(200).json(req.body)
+        const product = await prisma.product.create({
+            data: {
+                title: req.body.title,
+                description: req.body.description,
+                balance: req.body.balance,
+                minPrice: req.body.price,
+                maxPrice: req.body.price,
+            }
+        })
+        console.log(product)
+        res.status(200).json(product)
     } catch (error) {
         next(createError(error))
         console.log('error asdasads ', error)
