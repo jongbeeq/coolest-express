29c19846a10101baafad8796de049ee357e222c2
jongbeeq <titivutewhangsukij@gmail.com>
2024-05-24 01:28:24 +0700

    CreateProduct:CheckHaveType

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -116,116 +116,118 @@ exports.createProduct = async (req, res, next) => {
 
 
         // 1.Create Type
-        const productTypesPromise = req.body.types.map((title) => {
-            const createType = async () => {
-                const type = await prisma.productOptionalType.create({
-                    data: {
-                        title: title,
-                        productId: product.id
-                    }
-                })
-                const typeId = { [title]: type.id }
-                return typeId
-            }
-            return createType()
-        })
-
-        const productTypesId = (await Promise.all(productTypesPromise)).reduce((obj, type) => {
-            return { ...obj, ...type }
-        })
-
-        console.log("1 productTypes----", productTypesId)
-
-        // 2.Create Item
-        let productItemsPromise = []
-        for (let key in productTypesId) {
-            const items = req.body[`${key}/items`]
-            const itemsData = Array.isArray(items) ? items : [items]
-            const itemsPromise = itemsData.map((title) => {
-                const createItem = async () => {
-                    const item = await prisma.productOptionalItem.create({
+        if (req.body.types) {
+            const productTypesPromise = req.body.types.map((title) => {
+                const createType = async () => {
+                    const type = await prisma.productOptionalType.create({
                         data: {
                             title: title,
-                            productId: product.id,
-                            balance: +req.body[`${title}/balance`] || 0,
-                            price: +req.body[`${title}/price`] || +req.body.price,
-                            optionalTypeItems: {
-                                create: {
-                                    productOptionalTypeId: productTypesId[key]
-                                }
-                            },
+                            productId: product.id
                         }
                     })
-
-                    const itemId = { [`${key}-${title}`]: item.id }
-                    return itemId
+                    const typeId = { [title]: type.id }
+                    return typeId
                 }
-                return createItem()
+                return createType()
             })
-            productItemsPromise = [...productItemsPromise, ...itemsPromise]
-        }
 
-        const productItemsId = (await Promise.all(productItemsPromise)).reduce((obj, item) => {
-            return { ...obj, ...item }
-        })
-
-        console.log("2 productItemsId----", productItemsId)
-
-        // 3.Create Combine-item
-        const createCombineItem = async (combineItemsDataNum) => {
-            let productCombineItemsPromise = []
-            for (let key in combineItemsDataNum) {
-                const createCombineItems = async () => {
-                    const combineItem = await prisma.productOptionalItem.create({
-                        data: {
-                            title: key,
-                            balance: +req.body[`${key}/balance`] || 0,
-                            price: +req.body[`${key}/price`] || +req.body.price,
-                            productId: product.id,
-                            combineItem: {
-                                createMany: {
-                                    data: combineItemsDataNum[key].primaryId
-                                }
-                            }
-                        }
-                    })
-
-                    return { [key]: combineItem.id }
-                }
-                productCombineItemsPromise = [...productCombineItemsPromise, createCombineItems()]
-            }
-
-            const productCombineItemsId = (await Promise.all(productCombineItemsPromise)).reduce((obj, type) => {
+            const productTypesId = (await Promise.all(productTypesPromise)).reduce((obj, type) => {
                 return { ...obj, ...type }
             })
-            console.log("4 productCombineItemsId----", productCombineItemsId)
 
-            return productCombineItemsId
-        }
+            console.log("1 productTypes----", productTypesId)
 
-        let nextPrimaryItemsIdData = {}
-        for (let key in combineItemsData) {
-            const combineItemsField = key
-            const combineItemsFieldData = combineItemsData[combineItemsField]
-            const numCombineItem = combineItemsField.split('combineItems')[1]
-            for (let key in combineItemsFieldData) {
-                console.log("5.1 combineItemsFieldData----", combineItemsFieldData)
-                const combineItem = key
-                const combineItemData = combineItemsFieldData[combineItem]
-                console.log("5.2 combineItemData----", combineItemData)
-                let combineItemDataWithPID = []
-                console.log("5.3 nextPrimaryItemsIdData----", nextPrimaryItemsIdData)
-                for (let item of combineItemData.primaryId) {
-                    console.log("5.4 item in combineItemData----", item)
-                    const primaryItemId = { primaryId: numCombineItem > 1 ? nextPrimaryItemsIdData[item] : productItemsId[item] }
-                    combineItemDataWithPID = [...combineItemDataWithPID, primaryItemId]
-                }
-                combineItemsFieldData[combineItem].primaryId = combineItemDataWithPID
+
+            // 2.Create Item
+            let productItemsPromise = []
+            for (let key in productTypesId) {
+                const items = req.body[`${key}/items`]
+                const itemsData = Array.isArray(items) ? items : [items]
+                const itemsPromise = itemsData.map((title) => {
+                    const createItem = async () => {
+                        const item = await prisma.productOptionalItem.create({
+                            data: {
+                                title: title,
+                                productId: product.id,
+                                balance: +req.body[`${title}/balance`] || 0,
+                                price: +req.body[`${title}/price`] || +req.body.price,
+                                optionalTypeItems: {
+                                    create: {
+                                        productOptionalTypeId: productTypesId[key]
+                                    }
+                                },
+                            }
+                        })
+
+                        const itemId = { [`${key}-${title}`]: item.id }
+                        return itemId
+                    }
+                    return createItem()
+                })
+                productItemsPromise = [...productItemsPromise, ...itemsPromise]
             }
 
-            nextPrimaryItemsIdData = await createCombineItem(combineItemsFieldData)
-        }
+            const productItemsId = (await Promise.all(productItemsPromise)).reduce((obj, item) => {
+                return { ...obj, ...item }
+            })
 
+            console.log("2 productItemsId----", productItemsId)
+
+            // 3.Create Combine-item
+            const createCombineItem = async (combineItemsDataNum) => {
+                let productCombineItemsPromise = []
+                for (let key in combineItemsDataNum) {
+                    const createCombineItems = async () => {
+                        const combineItem = await prisma.productOptionalItem.create({
+                            data: {
+                                title: key,
+                                balance: +req.body[`${key}/balance`] || 0,
+                                price: +req.body[`${key}/price`] || +req.body.price,
+                                productId: product.id,
+                                combineItem: {
+                                    createMany: {
+                                        data: combineItemsDataNum[key].primaryId
+                                    }
+                                }
+                            }
+                        })
+
+                        return { [key]: combineItem.id }
+                    }
+                    productCombineItemsPromise = [...productCombineItemsPromise, createCombineItems()]
+                }
+
+                const productCombineItemsId = (await Promise.all(productCombineItemsPromise)).reduce((obj, type) => {
+                    return { ...obj, ...type }
+                })
+                console.log("4 productCombineItemsId----", productCombineItemsId)
+
+                return productCombineItemsId
+            }
+
+            let nextPrimaryItemsIdData = {}
+            for (let key in combineItemsData) {
+                const combineItemsField = key
+                const combineItemsFieldData = combineItemsData[combineItemsField]
+                const numCombineItem = combineItemsField.split('combineItems')[1]
+                for (let key in combineItemsFieldData) {
+                    console.log("5.1 combineItemsFieldData----", combineItemsFieldData)
+                    const combineItem = key
+                    const combineItemData = combineItemsFieldData[combineItem]
+                    console.log("5.2 combineItemData----", combineItemData)
+                    let combineItemDataWithPID = []
+                    console.log("5.3 nextPrimaryItemsIdData----", nextPrimaryItemsIdData)
+                    for (let item of combineItemData.primaryId) {
+                        console.log("5.4 item in combineItemData----", item)
+                        const primaryItemId = { primaryId: numCombineItem > 1 ? nextPrimaryItemsIdData[item] : productItemsId[item] }
+                        combineItemDataWithPID = [...combineItemDataWithPID, primaryItemId]
+                    }
+                    combineItemsFieldData[combineItem].primaryId = combineItemDataWithPID
+                }
+
+                nextPrimaryItemsIdData = await createCombineItem(combineItemsFieldData)
+            }
+        }
 
         // 4.Get Product
         const getProduct = await prisma.product.findUnique({
@@ -276,7 +278,7 @@ exports.createProduct = async (req, res, next) => {
         })
 
         getProduct.combineItems = getProduct.productOptionalItems
-        getProduct.productCategorys = getProduct.productCategorys.map((category) => category.categoryTitle)
+        getProduct.productCategorys = getProduct.productCategorys?.map((category) => category.categoryTitle)
         delete getProduct.productOptionalItems
         const respond = getProduct
         res.status(200).json(respond)
